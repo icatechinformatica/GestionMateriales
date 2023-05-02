9e0f932a92381829f378f6541671efb79822d6a3
danielmendez88 <daniboymendez@gmail.com>
2023-05-02 14:53:32 -0600

    modificaciones en CustomAuthController

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -19,7 +19,7 @@ class CustomAuthController extends Controller
     //
     public function index()
     {
-
+        dd($this->getVehiculo());
         $catalogo = $this->getVehiculo();
         return view('theme.dashboard.layouts.index_cat_automovil', compact('catalogo'));
     }
