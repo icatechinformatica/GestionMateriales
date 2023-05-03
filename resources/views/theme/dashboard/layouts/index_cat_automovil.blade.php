57ad774419eec8daee9a80acc5c6d2660b28f069
danielmendez88 <daniboymendez@gmail.com>
2023-05-02 21:26:28 -0600

    modificaciones en cat automovil

--- resources/views/theme/dashboard/layouts/index_cat_automovil.blade.php
+++ resources/views/theme/dashboard/layouts/index_cat_automovil.blade.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    HOLA MUNDO
-</body>
-</html>
+{{-- DISEÑADO Y DESARROLLADO POR MIS. DANIEL MÉNDEZ CRUZ --}}
+@extends('theme.dashboard.main')
+
+@section('title', 'Revisión de Bitácora | SISCOM by ICATECH')
+
+
+@section('contenido')
+<h1>HOLA MUNDO</h1>
+
+@endsection
+
+@section('contenidoJavaScript')
+
+@endsection
+{{-- DISEÑADO Y DESARROLLADO POR MIS. DANIEL MÉNDEZ CRUZ --}}
